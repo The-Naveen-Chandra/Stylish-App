2cccbec6783f3d1d0e56e4dec99b42894ddec944
Naveen Chandra <the.naveenchandra@gmail.com>
2023-05-30 16:04:45 +0530

    Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:stylish_app/constants/constants.dart';
+import 'package:stylish_app/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +13,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter ',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: "Gordita",
+        scaffoldBackgroundColor: bgColor,
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.black54),
+        ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const HomeScreen(),
     );
   }
 }
